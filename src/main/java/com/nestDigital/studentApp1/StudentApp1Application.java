6de28a986585e63dd92593ad9af6d7cb84a921f6
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-17 01:13:35 +0530

    api

--- src/main/java/com/nestDigital/studentApp1/StudentApp1Application.java
+++ src/main/java/com/nestDigital/studentApp1/StudentApp1Application.java
@@ -1,6 +1,6 @@
 package com.nestDigital.studentApp1;
 
-import giorg.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
--- src/main/java/com/nestDigital/studentApp1/controller/StudentController.java
+++ src/main/java/com/nestDigital/studentApp1/controller/StudentController.java
@@ -1,4 +1,14 @@
 package com.nestDigital.studentApp1.controller;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class StudentController {
+    @GetMapping("/")
+    public String homePage(){
+        return "Welcome to My Home Page";
+    }
+    @GetMapping("/contact")
+    public String Contact(){
+        return "this my Contact page";
+    }
 }
