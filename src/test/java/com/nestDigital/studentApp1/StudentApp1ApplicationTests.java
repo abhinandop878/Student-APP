b8c62d4adf57cb098bbb079b24f1f53c64bc696d
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-17 00:15:20 +0530

    Student App using spring

---
+++ src/test/java/com/nestDigital/studentApp1/StudentApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestDigital.studentApp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
